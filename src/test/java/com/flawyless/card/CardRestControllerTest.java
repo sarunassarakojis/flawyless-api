84871e5b50b428d6d767816f1b020378599d8426
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2017-10-09 22:18:29 +0300

    Add write test case

--- src/test/java/com/flawyless/card/CardRestControllerTest.java
+++ src/test/java/com/flawyless/card/CardRestControllerTest.java
@@ -12,17 +12,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,7 +38,7 @@ import static org.hamcrest.Matchers.*;
 public class CardRestControllerTest {
 
     private MockMvc mockMvc;
-    private HttpMessageConverter<?> messageConverter;
+    private HttpMessageConverter<Object> messageConverter;
     private List<Card> testCards;
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -42,8 +46,8 @@ public class CardRestControllerTest {
     private CardRepository cardRepository;
 
     @Autowired
-    void setMessageConverter(HttpMessageConverter<?>[] messageConverters) {
-        this.messageConverter = Arrays.asList(messageConverters).stream()
+    void setMessageConverter(HttpMessageConverter<Object>[] messageConverters) {
+        this.messageConverter = Arrays.stream(messageConverters)
                 .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
                 .findAny()
                 .orElseThrow(() -> new AssertionError("No JSON message converter found, aborting..."));
@@ -56,16 +60,17 @@ public class CardRestControllerTest {
 
         cardRepository.deleteAllInBatch();
         testCards.add(cardRepository.save(new Card("TEST_SUMMARY_0", "TEST_DESCRIPTION_0")));
-        testCards.add(cardRepository.save(new Card("TEST_SUMMARY_1", "TEST_DESCRIPTION_1")));
-        testCards.add(cardRepository.save(new Card("TEST_SUMMARY_2")));
+        testCards.add(cardRepository.save(new Card("TEST_SUMMARY_1")));
     }
 
     @Test
     public void attemptToReadAllCards() throws Exception {
-        mockMvc.perform(get(CardController.CARD_API))
-                .andExpect(status().isOk())
+        ResultActions resultActions = mockMvc.perform(get(CardController.CARD_API));
+
+        resultActions.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(jsonPath("$", hasSize(testCards.size())));
+        assertCardContentCorrectness(testCards, resultActions);
     }
 
     @Test
@@ -86,5 +91,37 @@ public class CardRestControllerTest {
 
         mockMvc.perform(get(CardController.CARD_API + "/" + (lastCardId + 1)))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void attemptToCreateNewCard() throws Exception {
+        String cardJson = jsonify(new Card("sample_summary", "sample_description"));
+
+        mockMvc.perform(post(CardController.CARD_API)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(cardJson))
+                .andExpect(status().isCreated());
+    }
+
+    private static void assertCardContentCorrectness(List<Card> expectedCards, ResultActions resultActions) throws Exception {
+        for (int i = 0, n = expectedCards.size(); i < n; i++) {
+            Card card = expectedCards.get(i);
+
+            resultActions.andExpect(jsonPath(createJsonPath(i, "id"), is((int) card.getId())))
+                    .andExpect(jsonPath(createJsonPath(i, "summary"), is(card.getSummary())))
+                    .andExpect(jsonPath(createJsonPath(i, "description"), is(card.getDescription())));
+        }
+    }
+
+    private static String createJsonPath(int objectIndex, String fieldName) {
+        return String.format("$[%d].%s", objectIndex, fieldName);
+    }
+
+    private String jsonify(Object o) throws IOException {
+        MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
+
+        messageConverter.write(o, MediaType.APPLICATION_JSON, outputMessage);
+
+        return outputMessage.getBodyAsString();
     }
 }
