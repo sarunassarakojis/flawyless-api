b38ee0cec9a89146200263b1a8fca28cbed51b79
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2017-11-14 01:24:57 +0200

    Add Oauth2 Authorization implementation

--- src/main/java/com/flawyless/ApplicationLauncher.java
+++ src/main/java/com/flawyless/ApplicationLauncher.java
@@ -1,12 +1,26 @@
 package com.flawyless;
 
+import com.flawyless.model.Card;
+import com.flawyless.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 
 @SpringBootApplication
 public class ApplicationLauncher {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationLauncher.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner addTestData(@Autowired CardService cardService) {
+        return args -> {
+            cardService.saveCard(new Card("test_summary_0", "test_desc_0"));
+            cardService.saveCard(new Card("test_summary_1", "test_desc_1"));
+        };
     }
 }
